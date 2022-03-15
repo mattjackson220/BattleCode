40c396538eb633070d5a05850534607ac1a1a107
mcreehan <mcreehan@alumni.nd.edu>
2022-03-15 08:50:23 -0600

    New Game files added, removing mcreeh200 specific files from tracking

---
+++ BattleCode iOS/New Game/NewGameViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  BattleCode Shared
+//
+//  Created by Matt Jackson on 3/14/22.
+//
+
+import UIKit
+
+class NewGameViewController: UIViewController {
+    
+    @IBOutlet weak var newGameButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
