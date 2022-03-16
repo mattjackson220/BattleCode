7f2f1aef7789a9c33e426c65e853b68d8a1f26eb
mattjackson220 <mjackso8@alumni.nd.edu>
2022-03-16 12:55:07 -0600

    fixed second page animation of numbers

--- BattleCode Shared/BaseViewController.swift
+++ BattleCode Shared/BaseViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class BaseViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         
         for _ in 0...500 {
             let randomIntX = Int.random(in: 10..<Int(self.view.frame.size.width-10))
