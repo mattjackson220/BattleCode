95bd51920ec9251e3cc21c98f7bc0e700739c71c
mattjackson220 <mjackso8@alumni.nd.edu>
2022-03-18 10:22:00 -0600

    separated out tutorial

---
+++ BattleCode iOS/TutorialViewController.swift
@@ -0,0 +1,12 @@
+//
+//  TutorialViewController.swift
+//  BattleCode iOS
+//
+//  Created by Matt Jackson on 3/18/22.
+//
+
+import Foundation
+
+class TutorialViewController: GameViewController {
+
+}
